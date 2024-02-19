834a33c2d0d05e890095dde0b149e6e81cb714b3
robotomize <robotomize@gmail.com>
2024-02-20 01:13:14 +0300

    upd clap and fix errors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use bf::BloomFilter;
-use clap::{App, Arg};
+use clap::{Arg, Command};
 
 fn main() {
-    let matches = App::new("Bloom Filter CLI")
+    let matches = Command::new("Bloom Filter CLI")
         .version("1.0")
         .author("Author Name <author@example.com>")
         .about("Demonstrates a basic Bloom Filter")
@@ -10,24 +10,22 @@ fn main() {
             .short('a')
             .long("add")
             .value_name("VALUE")
-            .help("Adds an item to the Bloom Filter")
-            .takes_value(true))
+            .help("Adds an item to the Bloom Filter"))
         .arg(Arg::new("check")
             .short('c')
             .long("check")
             .value_name("VALUE")
-            .help("Checks if an item is in the Bloom Filter")
-            .takes_value(true))
+            .help("Checks if an item is in the Bloom Filter"))
         .get_matches();
 
     let mut bloom_filter = BloomFilter::new(10000, 3);
 
-    if let Some(item) = matches.value_of("add") {
+    if let Some(item) = matches.get_one::<String>("add") {
         bloom_filter.add(item);
         println!("Value '{}' added to the Bloom Filter.", item);
     }
 
-    if let Some(item) = matches.value_of("check") {
+    if let Some(item) = matches.get_one::<String>("check") {
         if bloom_filter.contains(item) {
             println!("Value '{}' is probably in the Bloom Filter.", item);
         } else {
